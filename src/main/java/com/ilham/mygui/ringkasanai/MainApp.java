1c1f7aceab9fa1707c58e4ca43f2b34c8e138279
Muhammad Fajar Ilham <mfilham06@gmail.com>
2025-06-18 20:12:43 +0700

    fix: dapat membuat fat jar barjalan

---
+++ src/main/java/com/ilham/mygui/ringkasanai/MainApp.java
@@ -0,0 +1,9 @@
+package com.ilham.mygui.ringkasanai;
+
+import com.ilham.mygui.ringkasanai.app.Main;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
